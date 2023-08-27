e98c469dcfc977e053598115dfb5b1187e8778d3
nechiburu <137901882+nechiburu@users.noreply.github.com>
2023-08-27 17:14:34 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
